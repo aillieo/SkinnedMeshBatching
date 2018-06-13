f58e187ea12ec0119e2f100468dc6a743273b635
aillieo <aillieo@163.com>
2018-06-13 23:42:36 +0800

    update

---
+++ Assets/SkinnedMeshBatching/ArrayByInstancing.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayByInstancing : MonoBehaviour {
+
+	public SkinnedMeshRenderer skinnedMeshRenderer;
+	public GameObject refGameObject;
+	public Transform[] instances;
+	public float instanceScale = 1;
+
+	Mesh bakedSkinnedMesh;
+
+	Vector3[] positionOffset;
+	Matrix4x4[] translateMats;
+
+	int instanceCount = 0;
+	Vector3 scaleVec = Vector3.one;
+
+	Transform refTrans;
+
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("<color=#FF0000>材质是否支持 instancing " + skinnedMeshRenderer.sharedMaterial.enableInstancing + "</color>");
+
+		Debug.Log("<color=#FF0000>系统是否支持 instancing " + SystemInfo.supportsInstancing + "</color>");
+
+		InitComponent();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		skinnedMeshRenderer.BakeMesh(bakedSkinnedMesh);
+
+		UpdateMatrix();
+
+		Graphics.DrawMeshInstanced(bakedSkinnedMesh, 0, skinnedMeshRenderer.sharedMaterial, translateMats, translateMats.Length, null, UnityEngine.Rendering.ShadowCastingMode.Off, false);
+
+	}
+
+	void InitComponent()
+	{
+		refTrans = refGameObject.transform;
+		instanceCount = instances.Length;
+
+		scaleVec = instanceScale * Vector3.one;
+
+		translateMats = new Matrix4x4[instanceCount];
+		positionOffset = new Vector3[instanceCount];
+
+		for (int i = 0; i < instanceCount; i++)
+		{
+			positionOffset[i] = instances[i].position - refTrans.position;
+		}
+
+		bakedSkinnedMesh = new Mesh();
+
+	}
+		
+	void UpdateMatrix()
+	{
+		for (int i = 0; i < instanceCount; i++)
+		{
+			translateMats[i] = Matrix4x4.TRS(positionOffset[i] + refTrans.position, refTrans.rotation, scaleVec);
+		}
+	}
+}
---
+++ Assets/SkinnedMeshBatching/ArrayByMeshSharing.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayByMeshSharing : MonoBehaviour {
+
+	public SkinnedMeshRenderer skinnedMeshRenderer;
+	public GameObject[] instances;
+
+	Material matForArray;
+	Mesh meshForArray;
+
+	// Use this for initialization
+	void Start()
+	{
+		if(skinnedMeshRenderer != null)
+		{
+			InitComponent();
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		meshForArray.Clear();
+		skinnedMeshRenderer.BakeMesh(meshForArray);
+	}
+
+	void InitComponent()
+	{
+		matForArray = skinnedMeshRenderer.material;
+		meshForArray = new Mesh();
+
+		// 强行把instance的scale改为和mainObj一样大
+		Vector3 scale = skinnedMeshRenderer.gameObject.transform.lossyScale;
+		Vector3 localScale = new Vector3(1 / scale.x, 1 / scale.y, 1 / scale.z);
+
+		for (int i = 0; i < instances.Length; i++)
+		{
+			if(instances[i] != null)
+			{
+				InitInstance(instances[i], meshForArray, matForArray);
+				instances[i].transform.localScale = localScale;
+			}
+		}
+	}
+
+
+	void InitInstance(GameObject instance, Mesh mesh, Material mat)
+	{
+		MeshFilter mf = instance.GetComponent<MeshFilter>();
+		if (mf == null)
+		{
+			mf = instance.AddComponent<MeshFilter>();
+		}
+		mf.sharedMesh = meshForArray;
+
+		MeshRenderer mr = instance.GetComponent<MeshRenderer>();
+		if (mr == null)
+		{
+			mr = instance.AddComponent<MeshRenderer>();
+		}
+		mr.sharedMaterial = matForArray;
+	}
+}
